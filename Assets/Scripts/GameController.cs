4d1263ff51af8902863704561d8699f173af9f33
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2018-01-08 23:42:24 +0900

    local first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        HTTPClient.Instance.GET("http://127.0.0.1:8080/member", delegate (WWW www) {
+            Debug.Log(www.text);
+            User res = JsonUtility.FromJson<User>(www.text);
+            Debug.Log(res.bbbb);
+            Debug.Log(res.cccc);
+        });
+	}
+}
---
+++ Assets/Scripts/Model/Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class User
+{
+    public string bbbb;
+    public string cccc;
+
+    // http://127.0.0.1:8080/member
+    // {"bbbb":"111", "cccc":"2222"}
+};
